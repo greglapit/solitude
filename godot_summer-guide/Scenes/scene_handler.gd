9e5019a96c23bad1972d05cee8d4cf52b942822f
Gregory Lapit <greglapit@gmail.com>
2026-04-20 11:05:36 -0500

    jod wip lighting

--- godot_summer-guide/Scenes/scene_handler.gd
+++ godot_summer-guide/Scenes/scene_handler.gd
@@ -13,7 +13,7 @@ var loading_in_background : bool = false
 
 # DEV TOOLS
 #const starting_scn : PackedScene = main_menu_scn
-const starting_scn : PackedScene = preload("res://Scenes/EnteringSpread/entering_spread.tscn")
+const starting_scn : PackedScene = preload("res://Scenes/Encounters/JoDEncounter/jod_encounter.tscn")
 #const starting_scn : PackedScene = preload("res://Scenes/Battle/battle.tscn")
 #const starting_scn : PackedScene = preload("res://Scenes/TutorialBattle/tutorial_battle.tscn")
 #const starting_scn : PackedScene = preload("res://Scenes/Camp/camp.tscn")
