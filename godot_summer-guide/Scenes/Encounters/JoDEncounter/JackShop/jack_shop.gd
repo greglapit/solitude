757c1defe2f51599ed83e4c3ec35e1a691c6444f
Gregory Lapit <greglapit@gmail.com>
2026-04-29 11:40:16 -0500

    jack shop wip signal buying

--- godot_summer-guide/Scenes/Encounters/JoDEncounter/JackShop/jack_shop.gd
+++ godot_summer-guide/Scenes/Encounters/JoDEncounter/JackShop/jack_shop.gd
@@ -1,6 +1,10 @@
 class_name JackShop
 extends CanvasLayer
 
+@onready var item_list1 : ItemList = $JackShop/MarginContainer/PanelContainer/MarginContainer/HBoxContainer/VBoxContainer/MarginContainer/VBoxContainer/ItemList
+@onready var item_list2 : ItemList = $JackShop/MarginContainer/PanelContainer/MarginContainer/HBoxContainer/VBoxContainer/MarginContainer2/VBoxContainer/ItemList
+@onready var item_list3 : ItemList = $JackShop/MarginContainer/PanelContainer/MarginContainer/HBoxContainer/VBoxContainer2/MarginContainer/VBoxContainer/ItemList
+@onready var item_list4 : ItemList = $JackShop/MarginContainer/PanelContainer/MarginContainer/HBoxContainer/VBoxContainer2/MarginContainer2/VBoxContainer/ItemList
 @onready var animation_player : AnimationPlayer = $AnimationPlayer
 
 const jack_shop_scene : PackedScene = preload("res://Scenes/Encounters/JoDEncounter/jack_shop.tscn")
